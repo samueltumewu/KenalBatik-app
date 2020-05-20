d3d2b648edcc649234643f56c14bc064dfbca849
samueltumewu <samueltumewu21@gmail.com>
2020-05-20 11:51:01 +0700

    Update post url

--- src/app.jsx
+++ src/app.jsx
@@ -38,7 +38,7 @@ class App extends React.Component {
     this.setState({
       isLoading: 1
     });
-    const urlPredict = `http://192.168.1.12:5000/predict`;
+    const urlPredict = `http://localhost:5000/predict`;
 
     const formData = new FormData();
     formData.append('image', this.state.imageFile);
